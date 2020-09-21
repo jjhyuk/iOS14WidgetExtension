64327b07c199fe8686e24a8bc68750068b834321
jinhyuk <jjhyuk15@gmail.com>
2020-09-21 23:34:31 +0900

    Initial Commit

---
+++ WidgetSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetSample
+//
+//  Created by jinhyuk on 2020/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
